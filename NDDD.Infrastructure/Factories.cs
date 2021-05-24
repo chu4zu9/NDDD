97db9db336a647deab8e914c11dc3d44b4f28f5d
r_uesugi <r_uesugi@audio-technica.co.jp>
2021-05-24 16:58:55 +0900

    change:Fakeの生成がデバッグ時のみになるように処理変更.

--- NDDD.Infrastructure/Factories.cs
+++ NDDD.Infrastructure/Factories.cs
@@ -14,11 +14,12 @@ namespace NDDD.Infrastructure
     {
         public static IMeasureRepository CreateMeasure()
         {
+#if DEBUG
             if (Shared.IsFake)
             {
                 return new MeasureFake();
             }
-
+#endif
             return new MeasureSqlServer();
         }
     }
