80f757a412b65514cb3ace615c70ef88a36d412d
r_uesugi <r_uesugi@audio-technica.co.jp>
2021-05-24 15:55:08 +0900

    add:モックを使用して直近値取得処理を実装した.

--- NDDD.WinForm/ViewModels/LatestViewModel.cs
+++ NDDD.WinForm/ViewModels/LatestViewModel.cs
@@ -1,16 +1,59 @@
-﻿using System;
+﻿using NDDD.Domain.Entities;
+using NDDD.Domain.Repositories;
+using System;
 
 namespace NDDD.WinForm.ViewModels
 {
     public class LatestViewModel : ViewModelBase
     {
-        public object MeasureDateText { get; set; }
-        public object AreaIDText { get; set; }
-        public object MeasureValueText { get; set; }
+        private readonly IMeasureRepository _measureRepository;
+        private MeasureEntity _measure;
+
+        public LatestViewModel(IMeasureRepository measureRepository)
+        {
+            _measureRepository = measureRepository;
+        }
+
+        public string AreaIDText
+        {
+            get
+            {
+                if (_measure == null)
+                {
+                    return string.Empty;
+                }
+
+                return _measure.AreaID.ToString().PadLeft(4, '0');
+            }
+        }
+        public string MeasureDateText
+        {
+            get
+            {
+                if (_measure == null)
+                {
+                    return string.Empty;
+                }
+
+                return _measure.MeasureDate.ToString("yyyy/MM/dd hh:mm:ss");
+            }
+        }
+        public string MeasureValueText
+        {
+            get
+            {
+                if(_measure == null)
+                {
+                    return string.Empty;
+                }
+
+                return Math.Round(_measure.MeasureValue, 2) + "℃";
+            }
+        }
 
         public void Search()
         {
-            throw new NotImplementedException();
+            _measure = _measureRepository.GetLatest();
         }
     }
 }
--- NDDDTest.Tests/ViewModelTests/LatestViewModelTest.cs
+++ NDDDTest.Tests/ViewModelTests/LatestViewModelTest.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NDDD.Domain.Entities;
+using NDDD.Domain.Repositories;
 using NDDD.WinForm.ViewModels;
 using System;
 
@@ -10,8 +13,11 @@ namespace NDDDTest.Tests.ViewModelTests
         [TestMethod]
         public void シナリオ()
         {
-            var vm = new LatestViewModel();
+            var entity = new MeasureEntity(1, Convert.ToDateTime("2012/12/12 12:34:56"),12.341f);
+            var measureMock = new Mock<IMeasureRepository>();
+            var vm = new LatestViewModel(measureMock.Object);
 
+            measureMock.Setup(x => x.GetLatest()).Returns(entity);
             vm.Search();
             vm.AreaIDText.Is("0001");
             vm.MeasureDateText.Is("2012/12/12 12:34:56");
