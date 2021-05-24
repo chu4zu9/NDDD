f392aacc8904c8d60fb0bb922007c694527b91ff
r_uesugi <r_uesugi@audio-technica.co.jp>
2021-05-24 15:25:37 +0900

    add:LatestViewModelTest.csを追加. LatestViewModelをテストするためのソースを追加した.

---
+++ NDDDTest.Tests/ViewModelTests/LatestViewModelTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace NDDDTest.Tests.ViewModelTests
+{
+    [TestClass]
+    public class LatestViewModelTest
+    {
+        [TestMethod]
+        public void シナリオ()
+        {
+        }
+    }
+}
