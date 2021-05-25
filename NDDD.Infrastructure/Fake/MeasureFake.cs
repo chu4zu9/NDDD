c2c950620dcdc7d5bf07e3f364c4cb7e80ef3350
r_uesugi <r_uesugi@audio-technica.co.jp>
2021-05-25 15:30:32 +0900

    change:Fakeの値を外部ファイルから参照できるようにした.

--- NDDD.Infrastructure/Fake/MeasureFake.cs
+++ NDDD.Infrastructure/Fake/MeasureFake.cs
@@ -1,10 +1,7 @@
-﻿using NDDD.Domain.Entities;
+﻿using NDDD.Domain;
+using NDDD.Domain.Entities;
 using NDDD.Domain.Repositories;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NDDD.Infrastructure.Fake
 {
@@ -12,10 +9,23 @@ namespace NDDD.Infrastructure.Fake
     {
         public MeasureEntity GetLatest()
         {
-            return new MeasureEntity(
+            try
+            {
+                var lines = System.IO.File.ReadAllLines(Shared.FakePath + "MeasureFake.csv");
+                var value = lines[0].Split(',');
+                return new MeasureEntity(
+                    Convert.ToInt32(value[0]),
+                    Convert.ToDateTime(value[1]),
+                    Convert.ToSingle(value[2]));
+
+            }
+            catch
+            {
+                return new MeasureEntity(
                 1,
                 Convert.ToDateTime("2012/12/12 12:34:56"),
                 12.341f);
+            }
         }
     }
 }
